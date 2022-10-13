431bd6cafa105e787fccfc3ddfff7b839f77cedb
shiyivei <shiyivei@outlook.com>
2022-10-13 23:46:08 +0800

    fix utilis

--- src/components/utilis/contract_address.tsx
+++
@@ -1,5 +0,0 @@
-// # contract address
-const contract_address: string =
-  "0x17f3AcCddd7f424c8a207681bb945651fbD6aEB8";
-
-export default contract_address;
--- src/components/utilis/env.tsx
+++
@@ -1,7 +0,0 @@
-// # private key
-const PRIVATE_KEY = {
-  private_key:
-    "b0531e2f34c749fbe35e931729b0ea5fa8dc3e8c721959bff26b75072eab803f",
-};
-
-export default PRIVATE_KEY;
