83b818a8c5113915f338d4fd3d46e64373760a6a
shiyivei <shiyivei@outlook.com>
2022-10-14 16:49:56 +0800

    test all api successfully

--- src/components/Reward.tsx
+++ src/components/Reward.tsx
@@ -34,7 +34,7 @@ const web3 = new Web3(
 
 const contract_address: any = CONTRACT_ADDRESS;
 
-console.log("合约地址是:", contract_address);
+console.log("1.合约地址是:", contract_address);
 
 //合约ABI
 const abi: any = ABI;
@@ -45,18 +45,20 @@ const reward = new web3.eth.Contract(
   contract_address
 );
 
+//2.获取合约信息
+console.log("2.创建的合约实例:", reward);
+
 //1.查看合约创建者
 const get_owner = async () => {
-  var owner = await reward.methods.owner().call();
-  console.log("合约创建者:", `${owner}`);
+  var owner = await reward.methods.contract_owner().call();
+  console.log("3.合约创建者:", `${owner}`);
 
   return owner;
 };
 
 get_owner();
 
-//2.获取合约信息
-console.log("创建的合约实例:", reward);
+
 
 //2.查询创建者账户余额
 const test_address =
@@ -70,7 +72,7 @@ const get_iotex_test_balance = async () => {
         10 ** 18
       );
       console.log(
-        "%s 账户余额: %s IOTX",
+        "4.%s 账户余额: %s IOTX",
         test_address,
         iotxBalance.toFixed(2)
       );
@@ -80,22 +82,31 @@ const get_iotex_test_balance = async () => {
   return result;
 };
 
-let res = get_iotex_test_balance();
-let sss = res.then((s) => {
-  console.log(
-    "----------------------------------------------------------------",
-    s
-  );
-  return s;
-});
+get_iotex_test_balance();
 
-//3.查询合约某个报告是否已经存储（出具）
+//3.查询当前合约余额
+
+const token_address = TOKEN_ADDRESS;
+const get_contractBalance = async () => {
+  let result = await reward.methods
+    .get_contract_balance(token_address)
+    .call();
+
+  let balance:number = result;
+  console.log("5.当前合约余额:",Big(balance).div(
+    10 ** 18
+  ).toFixed(0));
+};
+
+get_contractBalance();
+
+//4.查询合约某个报告是否已经存储（出具）
 
 const get_report_hash_store_status = async (
   report_hash: string
 ) => {
   let result = await reward.methods
-    .get_reportHash_store_status(report_hash)
+    .report_is_stored(report_hash)
     .call();
   console.log(
     "未病测评报告时是否存储:",
@@ -103,28 +114,18 @@ const get_report_hash_store_status = async (
   );
 };
 
-//4.查询某个手表是否存储在合约中
+//5.查询某个手表是否存储在合约中
 
 const get_watch_store_status = async (
   imei: string
 ) => {
   let result = await reward.methods
-    .get_watch_store_status(imei)
+    .watch_is_stored(imei)
     .call();
+
+
   console.log("脉诊手表是否存储:", `${result}`);
 };
-
-//5.查询当前合约余额
-
-const token_address = TOKEN_ADDRESS;
-const get_contractBalance = async () => {
-  let result = await reward.methods
-    .getContractBalance(token_address)
-    .call();
-  console.log("当前合约余额:", `${result}`);
-};
-
-get_contractBalance();
 
 //6.存储报告哈希
 
@@ -170,13 +171,28 @@ const store_reportHash = async (
     });
 };
 
-console.log("获取到的随机报告哈希是:", r_hash);
 
-// get_report_hash_store_status(r_hash);
+//7.存储报告哈希
 
-// store_reportHash(r_hash);
+const get_reward_amount = async () => {
+  let result = await reward.methods
+    .reward_amount()
+    .call();
 
-// get_report_hash_store_status(r_hash);
+    console.log("当前提现金额:",Big(result).div(
+      10 ** 18
+    ).toFixed(0));
+};
+
+const get_next_reward_time = async (imei:string) => {
+  let result = await reward.methods
+    .get_next_reward_time(imei)
+    .call();
+  console.log(
+    "6.下次提现时间:",
+    `${result}`
+  );
+};
 
 //7 存储手表
 
@@ -214,16 +230,19 @@ const store_watch = async (imei: string) => {
     .sendSignedTransaction(signed.rawTransaction)
     .on("receipt", (receipt) => {
       console.log("store_watch receipt", receipt);
-      console.log("store_watch finished");
+      console.log("%s stored:",imei);
     });
+
+  return imei;
 };
 
-const get_next_reward_time = async (
-  r_hash: string
-) => {
-  console.log(
-    "call get_next_reward_time function......"
-  );
+console.log("获取到的随机IMEI码:", imei);
+
+// 转账
+
+
+const set_reward_amount = async (amount:number) => {
+  console.log("call set amount function......");
 
   const address =
     "0xFb7032b3fcfFc0A41E96B99AFd663A477819667C";
@@ -234,7 +253,7 @@ const get_next_reward_time = async (
     to: contract_address,
     gas: 5000000,
     data: reward.methods
-      .get_next_reward_time(r_hash)
+      .set_reward_amount(amount)
       .encodeABI(),
   };
 
@@ -253,31 +272,16 @@ const get_next_reward_time = async (
   web3.eth
     .sendSignedTransaction(signed.rawTransaction)
     .on("receipt", (receipt) => {
-      console.log("store_watch receipt", receipt);
-      console.log(
-        "get_next_reward_time finished"
-      );
+      console.log("set reward amount receipt", receipt);
+      console.log("set_reward_amount finished");
     });
 };
 
-console.log("获取到的随机IMEI码:", imei);
+const to: any =
+  "0xec876F62798E65270Ef163f86ec7afE5E7D634e7";
 
-// get_watch_store_status(imei);
-// store_watch(imei);
-// get_watch_store_status(imei);
-
-// 转账
-
-const transfer_erc20 = async (
-  token_address: any,
-  amount: number,
-  imei: string,
-  r_hash: string,
-  to: any
-) => {
-  console.log(
-    "call transfer_erc20 function......"
-  );
+const transfer_erc20 = async (token_contract_address:string, watch_id:string,report_hash:string,receive_address:string) => {
+  console.log("call set amount function......");
 
   const address =
     "0xFb7032b3fcfFc0A41E96B99AFd663A477819667C";
@@ -286,15 +290,9 @@ const transfer_erc20 = async (
   const tx: any = {
     from: address,
     to: contract_address,
-    gas: 500000,
+    gas: 5000000,
     data: reward.methods
-      .transferERC20(
-        token_address,
-        amount,
-        imei,
-        r_hash,
-        to
-      )
+      .transfer_erc20(token_contract_address,watch_id,report_hash,receive_address)
       .encodeABI(),
   };
 
@@ -313,38 +311,26 @@ const transfer_erc20 = async (
   web3.eth
     .sendSignedTransaction(signed.rawTransaction)
     .on("receipt", (receipt) => {
-      console.log(
-        "transfer_erc20 receipt",
-        receipt
-      );
-      console.log("transfer_erc20 finished");
+      console.log("set reward amount receipt", receipt);
+      console.log("set_reward_amount finished");
     });
 };
 
-const to: any =
-  "0xec876F62798E65270Ef163f86ec7afE5E7D634e7";
+const amount:number = 100000;
+const set_amount = Big(amount).times(10 ** 18).toFixed(0);
 
-// store_watch("watch2");
-// store_reportHash("hash2");
+// store_watch("watch3");
+// store_reportHash("hash3");
+// set_reward_amount(set_amount);
+// transfer_erc20(token_address,"watch3","hash3",to);
 
-get_watch_store_status("watch2");
-get_report_hash_store_status("hash2");
+// get_reward_amount();
+// get_watch_store_status("watch3");
+// get_report_hash_store_status("hash3");
+get_next_reward_time("watch3");
 
-const value = async () => {
-  let val = await get_next_reward_time("hash2");
 
-  console.log(
-    "----------------------------------------------------------------",
-    val
-  );
-};
 
-value();
 
-// transfer_erc20(
-//   token_address,
-//   100,
-//   "watch2",
-//   "hash2",
-//   to
-// );
+
+
